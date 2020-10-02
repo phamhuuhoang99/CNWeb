05cc560771370e59cafe090129d70300fd2de691
Huu Hoang <phamhuuhoang99@gmail.com>
2020-10-02 17:02:54 +0700

    Hoang

---
+++ CNWeb/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Womens()
+        {          
+            return View();
+        }
+        public ActionResult Men()
+        {
+            return View();
+        }
+    }
+}
---
+++ CNWeb/Models/Contact.cs
@@ -0,0 +1,29 @@
+namespace CNWeb.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Contact")]
+    public partial class Contact
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int id { get; set; }
+
+        [StringLength(50)]
+        public string name { get; set; }
+
+        [StringLength(50)]
+        public string phone { get; set; }
+
+        [StringLength(50)]
+        public string email { get; set; }
+
+        [StringLength(50)]
+        public string message { get; set; }
+
+        public DateTime? create_at { get; set; }
+    }
+}
